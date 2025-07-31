18d313f7f065b416694bce9d5808c84f88255beb
Ashmit <115689443+githubashmit@users.noreply.github.com>
2025-07-31 12:36:45 +0530

    MajorityElement

---
+++ src/L169_MajorityElement.java
@@ -0,0 +1,17 @@
+package src;
+import java.util.*;
+class L169_MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer , Integer> map =new HashMap<>();
+        int n = nums.length/2;
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer , Integer> entry : map.entrySet()){
+            if(entry.getValue()>n){
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
