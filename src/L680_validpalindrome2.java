71fb2010b1a5848b360fbea0ab1998292f4cd8c0
Ashmit <115689443+githubashmit@users.noreply.github.com>
2025-08-19 01:30:59 +0530

    L680_validpalindrome2

---
+++ src/L680_validpalindrome2.java
@@ -0,0 +1,30 @@
+package src;
+
+public class L680_validpalindrome2 {
+    public boolean isPalindrome(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j)
+        {
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+        }
+        return true;
+    }
+}
