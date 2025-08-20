1cff1d7c4fef91a06c14dbbcf2895843d07f8675
Ashmit <115689443+githubashmit@users.noreply.github.com>
2025-08-21 02:38:12 +0530

    L219_contains_duplicate2

---
+++ src/L219_Contains_Duplicate2.java
@@ -0,0 +1,20 @@
+package src;
+import java.util.*;
+public class L219_Contains_Duplicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(mp.containsKey(nums[i])){
+                int idx = mp.get(nums[i]);
+                if((i-idx)<=k){
+                    return true;
+                }else{
+                    mp.put(nums[i],i);
+                }
+            }else{
+                mp.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
