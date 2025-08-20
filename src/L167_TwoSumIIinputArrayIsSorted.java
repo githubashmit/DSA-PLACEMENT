30a175b65fe9947284f5a050def1fa8c921dde1d
Ashmit <115689443+githubashmit@users.noreply.github.com>
2025-08-20 23:49:38 +0530

    twosum2sortedarray

---
+++ src/L167_TwoSumIIinputArrayIsSorted.java
@@ -0,0 +1,25 @@
+package src;
+
+public class L167_TwoSumIIinputArrayIsSorted {
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int i=0;
+            int j=numbers.length-1;
+            int arr[]= new int[2];
+            while(i<j){
+                int sum = numbers[i]+numbers[j];
+                if(sum==target){
+                    arr[0]=i+1;
+                    arr[1]=j+1;
+                    break;
+                }
+                if(sum<target){
+                    i++;
+                }else{
+                    j--;
+                }
+            }
+            return arr;
+        }
+    }
+}
